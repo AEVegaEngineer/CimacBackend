06a6aace4800304d659bdaafa27d4c3e1afadbe1
unknown <alexissa41@gmail.com>
2020-08-28 00:20:11 -0300

    JWT IMPLEMENTADO

--- src/connection/Conexion.js
+++ src/connection/Conexion.js
@@ -1,24 +1,33 @@
- function consultar(){
-  var mysql      = require('mysql');
-  var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database: 'cimac'
-  });
+module.exports = {consultar}
+    //DATOS DE CONEXION
+    let mysql      = require('mysql');
+    let connection = mysql.createConnection({
+      host     : 'localhost',
+      user     : 'root',
+      password : '',
+      database: 'cimac'
+    });
 
+  function consultar (campos,tabla,where){
   connection.connect();
-  connection.query('SELECT user, pass from login_users', function(err, rows, fields) {
+  connection.query('SELECT '+campos+' from '+tabla+' WHERE '+where, function(err, rows, fields) {
       if (err) throw err;
       //  console.log('The solution is: ', rows);
        return JSON.stringify(rows)
     });
-    
-    connection.end();
+  connection.end();
+    //  return  rows.solution.json()
+}
+function Update (){
+  connection.connect();
+  connection.query('SELECT user, pass from login_users', function(err, rows, fields) {
+      if (err) throw err;
+      //  console.log('The solution is: ', rows);
+      console.log(JSON.stringify(rows))
+       return JSON.stringify(rows)
+    });
+  connection.end();
     //  return  rows.solution.json()
 }
 
 
-module.exports={
-  consultar
-}
--- src/server/app.js
+++ src/server/app.js
@@ -1,65 +1,102 @@
-const express = require('express')
-const app = express()
-const port = 3000
-var bodyParser = require('body-parser')
-var conn = require('../connection/Conexion')
+const { response } = require('express');
 
-// PARA CORRER EL SERVIDOR EN MODO TESTING ESCUCHA TIEMPO REAL ESCRIBIR... "npm run dev"
-// create application/json parser
-var jsonParser = bodyParser.json()
- 
-// create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-  const saludar = {
-    saludo:"holaa",
+const express = require('express'),
+      conn = require('../connection/Conexion'),
+      bodyParser = require('body-parser'),
+      jwt = require('jsonwebtoken'),
+      config = require('../configs/config'),
+      app = express();
+// 1
+app.set('llave', config.llave);
+// 2
+app.use(bodyParser.urlencoded({ extended: true }));
+// 3
+app.use(bodyParser.json());
+// 4
+app.listen(3000,()=>{
+    console.log('Servidor iniciado en el puerto 3000') 
+});
+// 5
+app.get('/', function(req, res) {
+  const saludar={
+    mensaje:"Hola, bienvenido a la API DE CIMAC. Para consultar por endpoints ponte en contacto con Alexissa41@gmail.com"
   }
-  res.sendStatus='Ok'
-  res.statusCode=200
-  res.json(saludar)
+    res.status=200
+    res.send(JSON.stringify( saludar));
+});
+app.post('/', function(req, res) {
+  const saludar={
+    mensaje:"Hola, bienvenido a la API DE CIMAC. Para consultar por endpoints ponte en contacto con Alexissa41@gmail.com"
+  }
+    res.status=200
+    res.send(JSON.stringify( saludar));
+});
+ 
+
+const rutasProtegidas = express.Router(); 
+rutasProtegidas.use((req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];    
+    if (token) {
+      jwt.verify(token, app.get('llave'), (err, decoded) => {      
+        if (err) {
+          return res.json({ mensaje: 'Token inválida' });    
+        } else {
+          req.decoded = decoded;    
+          next();
+        }
+      });
+    } else {
+      res.send({ 
+          mensaje: 'Token vacio' 
+      });
+    }
+  } catch (error) {
+    console.log(error)
+    res.send({ 
+      mensaje: 'Se recibio un formato de token invalido' 
+  });
+  }
+ 
+   
+ });
+
+app.post('/autenticar', (req, res) => {
+    if(req.body.usuario === "asfo" && req.body.contrasena === "holamundo") {
+  const payload = {
+  check:  true
+  };
+  const token = jwt.sign(payload, app.get('llave'));
+  res.json({
+  mensaje: 'Autenticación correcta',
+  token: token
+  });
+    } else {
+        res.json({ mensaje: "Usuario o contraseña incorrectos"})
+    }
 })
 
-app.get('/getUsers',(req, res)=>{
- const arr = conn.consultar()
- console.log(arr)
-  console.log(conn.consultar())
+app.get('/datos', rutasProtegidas, (req, res) => {
+  const datos = [
+   { id: 1, nombre: "Asfo" },
+   { id: 2, nombre: "Denisse" },
+   { id: 3, nombre: "Carlos" }
+  ];
   
-  res.sendStatus=200
-  // res.json(conn.consultar())
-  res.json({"saludo":"hola"})
-})
+  res.json(datos);
+ });
 
-app.post('/startURL', urlencodedParser,jsonParser, function(req, res){
-  // var content = JSON.stringify(req.body)
-console.log(req.body)
-  // if(content.name === 'Alexis')
-  // {
-  //   console.log("Mandaste parametro de Alexis")
-  // }
-  // else {
-  //   console.log("Otro parametro: "+content.name)
-  // }
-  res.sendStatus=200
-  // res.json(JSON.stringify(content.name))
-   res.json(req.body)
-})
-app.post('/startJSONPARSER', jsonParser, function(req, res){
-  // var content = JSON.stringify(req.body)
-console.log(req.body)
-  // if(content.name === 'Alexis')
-  // {
-  //   console.log("Mandaste parametro de Alexis")
-  // }
-  // else {
-  //   console.log("Otro parametro: "+content.name)
-  // }
-  res.sendStatus=200
-  // res.json(JSON.stringify(content.name))
-   res.json(req.body)
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+ app.get('/getUsers', rutasProtegidas, (req, res)=>{
+  
+   const arr = conn.consultar('user,pass','login_users','user = "'+req.body.user+'"')
+   //console.log(arr)
+   // console.log(conn.consultar())
+  //  console.log(arr)
+  console.log(arr)
+   res.sendStatus=200
+   // res.json(conn.consultar())
+   
+   res.json({"saludo":"hola"})
+ })
+ 
