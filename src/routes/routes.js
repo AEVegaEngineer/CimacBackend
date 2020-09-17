c024d3e2e27bba9d3decd69cade28258538a9fa4
unknown <alexissa41@gmail.com>
2020-09-17 18:53:52 -0300

    cambios y os agregado

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,45 +3,48 @@ var router = express.Router();
 const app = express();
 const autenticar = require("../routes/autenticar/autenticar");
 const moment = require("moment");
+const obras_sociales = require("../routes/obras_sociales/obras_sociales");
 
 //le asignamos la seguridad a las rutas
 const security = require("../configs/security");
 
 //Middle ware that is specific to this router
 router.use(function timeLog(req, res, next) {
-  console.log("Time: ", moment().format());
-  next();
+    console.log("Time: ", moment().format());
+    next();
 });
 
 // Define the home page route
 router.post("/", function (req, res) {
-  const saludar = {
-    mensaje:
-      "Hola, bienvenido a la API de CIMAC S.A. Para consultar por endpoints ponte en contacto con alexissa41@gmail.com (Programador de Backend)",
-  };
-  res.status = 200;
-  res.send(JSON.stringify(saludar));
+    const saludar = {
+        mensaje:
+            "Hola, bienvenido a la API de CIMAC S.A. Para consultar por endpoints ponte en contacto con alexissa41@gmail.com (Programador de Backend)",
+    };
+    res.status = 200;
+    res.send(JSON.stringify(saludar));
 });
 
 router.get("/", function (req, res) {
-  const saludar = {
-    mensaje:
-      "Hola, bienvenido a la API de CIMAC S.A. Para consultar por endpoints ponte en contacto con alexissa41@gmail.com (Programador de Backend)",
-  };
-  res.status = 200;
-  res.send(JSON.stringify(saludar));
+    const saludar = {
+        mensaje:
+            "Hola, bienvenido a la API de CIMAC S.A. Para consultar por endpoints ponte en contacto con alexissa41@gmail.com (Programador de Backend)",
+    };
+    res.status = 200;
+    res.send(JSON.stringify(saludar));
 });
 
 router.post("/autenticar", autenticar);
 
+router.get("/getObrasSociales", security, obras_sociales);
+
 //Example Route
 router.get("/datos", security, (req, res) => {
-  const datos = [
-    { id: 1, nombre: "Asfo" },
-    { id: 2, nombre: "Denisse" },
-    { id: 3, nombre: "Carlos" },
-  ];
-  res.json(datos);
+    const datos = [
+        { id: 1, nombre: "Asfo" },
+        { id: 2, nombre: "Denisse" },
+        { id: 3, nombre: "Carlos" },
+    ];
+    res.json(datos);
 });
 
 module.exports = router;
